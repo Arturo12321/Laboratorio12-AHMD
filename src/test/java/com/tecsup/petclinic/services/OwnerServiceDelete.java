ceea5d3023307835f6dae38dc6013a52e310e42f
Nanathaly <marce.amao97@gmail.com>
2022-11-08 23:25:13 -0500

    Cambio de Amao - Delete

---
+++ src/test/java/com/tecsup/petclinic/services/OwnerServiceDelete.java
@@ -0,0 +1,60 @@
+package com.tecsup.petclinic.services;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.tecsup.petclinic.entities.Owner;
+import com.tecsup.petclinic.entities.Pet;
+import com.tecsup.petclinic.exception.OwnerNotFoundException;
+import com.tecsup.petclinic.exception.PetNotFoundException;
+
+@SpringBootTest
+public class OwnerServiceDelete {
+	
+	private static final Logger logger = LoggerFactory.getLogger(OwnerServiceDelete.class);
+
+	@Autowired
+	private OwnerService ownerService;
+	
+	@Test
+	public void testDeleteOwner() {
+
+		String FIRST_NAME = "Rosa";
+		String LAST_NAME = "Gutierrez";
+		String ADDRESS = "Jr.Valentin 234.";
+		String CITY = "California";
+		String TELEPHONE = "77678673";
+
+		Owner owner = new Owner(FIRST_NAME,LAST_NAME,ADDRESS,CITY,TELEPHONE);
+		owner = ownerService.create(owner);
+		logger.info("" + owner);
+		
+		try {
+			ownerService.delete(owner.getId());
+		} catch (OwnerNotFoundException e) {
+			fail(e.getMessage());
+		}
+			
+		try {
+			ownerService.findById(owner.getId());
+			fail("Owner id = " + owner.getId() + " has not delete");
+		} catch (OwnerNotFoundException e) {
+		} 				
+	}
+}
+
+	
